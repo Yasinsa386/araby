e3a37d980ce6e14886edf99d655a7916748b69b0
Yasin <Yasinsa386@gmail.com>
2024-01-16 20:22:16 +0330

    initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<html lang="fa-ir">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>ریدم به عربی</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/Font/font.css">
+</head>
+<style>
+    body{
+        font-size: 16px;
+    }
+</style>
+<body dir="rtl" class="sans">
+
+    <?php
+    $str = "عربی";
+    for ($i = 0; $i < 300; $i++) {
+        echo $str = "ریدم به " . $str;
+        echo "<br><br><br><br><br><br><br><br>";
+    }
+    ?>
+</body>
+
+</html>
